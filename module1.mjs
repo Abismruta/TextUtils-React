e9e5e0e6e5d23f8f95d94e650e706352795633c4
Abismruta Sasamal <simunabismruta.2014@gmail.com>
2022-03-10 12:44:56 +0530

    TextUtits-done

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+// import ui from './module2.mjs';
+// console.log(ui);
+// import dza from './module2.mjs';
+// console.log(dza);
+import dza, {a, c, d} from './module2.mjs';
+console.log(dza);
+console.log(c);
+console.log(d);
+console.log(a);
